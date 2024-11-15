67b7bb32690559720807ebe709ff5498f374d12a
Marco Michelino <marco.michelino@immobiliare.it>
2024-11-15 11:29:59 +0100

    you can't save an Rc in a KVS

--- hooks/limit_buy/src/limit_buy.rs
+++ hooks/limit_buy/src/limit_buy.rs
@@ -8,7 +8,7 @@ use crate::order::*;
 
 // NonFungibleData for the limit buy order NFT
 #[derive(ScryptoSbor, NonFungibleData)]
-struct LimitBuyOrderData {
+struct BuyOrderData {
     date_created: Instant,
     base_coin_amount: Decimal,
     coin_to_buy: ResourceAddress,
@@ -23,19 +23,15 @@ struct MatchedOrderEvent {
     partially_filled_orders_id: Option<u32>,
 }
 
-// Limits to avoid transaction fees can grow too much
-static MAX_MATCHING_ORDERS: usize = 50;
-static MAX_ACTIVE_ORDERS_PER_COIN: usize = 500;
-
 #[blueprint_with_traits]
 #[events(MatchedOrderEvent)]
 #[types(
     ResourceAddress,
     u32,
-    LimitBuyOrder,
-    Vec<LimitBuyOrderRef>,
+    OrderRef,
+    OrderBook,
     Vault,
-    LimitBuyOrderData,
+    BuyOrderData,
 )]
 mod limit_buy_hook {
 
@@ -62,13 +58,7 @@ mod limit_buy_hook {
         // The numeric id of the last created order
         last_order_id: u32,
 
-        // This KVS contains information on the base coin and coin amounts of all of the present and past orders
-        orders: KeyValueStore<u32, LimitBuyOrder>,
-
-        // This KVS contains one order book for each coin
-        // In this simple implementation the order book is just a vector sorted by decreasing price
-        // and increasing id
-        active_orders: KeyValueStore<ResourceAddress, Vec<LimitBuyOrderRef>>,
+        order_books: KeyValueStore<ResourceAddress, OrderBook>,
 
         // The address of the RadixPump component, it is used to perform some checks when a new
         // order is created
@@ -96,11 +86,11 @@ mod limit_buy_hook {
 
         ) -> Global<LimitBuyHook> {
 
-            // Reserve a component address to set proper permissions on the LimitBuyOrder NFT
+            // Reserve a component address to set proper permissions on the BuyOrder NFT
             let (address_reservation, component_address) = Runtime::allocate_component_address(LimitBuyHook::blueprint_id());
 
-            // Create a resource manager to mint LimitBuyOrder NFTs
-            let orders_resource_manager = ResourceBuilder::new_integer_non_fungible_with_registered_type::<LimitBuyOrderData>(
+            // Create a resource manager to mint BuyOrder NFTs
+            let orders_resource_manager = ResourceBuilder::new_integer_non_fungible_with_registered_type::<BuyOrderData>(
                 OwnerRole::Updatable(rule!(require(owner_badge_address)))
             )
             .mint_roles(mint_roles!(
@@ -129,8 +119,7 @@ mod limit_buy_hook {
                 base_coin_vault: Vault::new(base_coin_address),
                 orders_resource_manager: orders_resource_manager,
                 last_order_id: 0,
-                orders: KeyValueStore::new_with_registered_type(),
-                active_orders: KeyValueStore::new_with_registered_type(),
+                order_books: KeyValueStore::new_with_registered_type(),
                 radix_pump_component: Global::from(radix_pump_component),
                 coins_vaults: KeyValueStore::new_with_registered_type(),
             }
@@ -226,42 +215,26 @@ mod limit_buy_hook {
                 }
             }
 
-            // Create a LimitBuyOrder object and store it in the KVS
             self.last_order_id += 1;
-            let order = LimitBuyOrder::new(base_coin_bucket.amount());
-            self.orders.insert(
-                self.last_order_id,
-                order,
-            );
-
-            // Create a LimitBuyOrderRef object too and add it to the active orders
-            let order_ref = LimitBuyOrderRef::new(
+            let order = OrderRef::new(
                 self.last_order_id,
                 price,
+                base_coin_bucket.amount(),
             );
-            let mut active_orders = self.active_orders.get_mut(&coin_to_buy);
-            match active_orders {
+
+            let mut order_book = self.order_books.get_mut(&coin_to_buy);
+            match order_book {
                 None => {
-                    drop(active_orders);
+                    drop(order_book);
 
                     // Create an order book with just this order in it
-                    self.active_orders.insert(
+                    self.order_books.insert(
                         coin_to_buy,
-                        vec![order_ref]
+                        OrderBook::new(order)
                     );
                 },
-                Some(ref mut active_orders) => {
-                    assert!(
-                        active_orders.len() < MAX_ACTIVE_ORDERS_PER_COIN,
-                        "This orderbook is full",
-                    );
-
-                    // The order book is sorted by decreasing price and increasing id
-                    // Find the right place to insert the new order
-                    match active_orders.binary_search(&order_ref) {
-                        Ok(_) => Runtime::panic("Should not happen".to_string()),
-                        Err(pos) => active_orders.insert(pos, order_ref),
-                    }
+                Some(ref mut order_book) => {
+                    order_book.insert(order);
                 }
             }
 
@@ -269,7 +242,7 @@ mod limit_buy_hook {
             // it to the vec of buckets for the user
             let order_nft = self.orders_resource_manager.mint_non_fungible(
                 &NonFungibleLocalId::integer(self.last_order_id.into()),
-                LimitBuyOrderData {
+                BuyOrderData {
                     date_created: Clock::current_time_rounded_to_seconds(),
                     base_coin_amount: base_coin_bucket.amount(),
                     coin_to_buy: coin_to_buy,
@@ -298,49 +271,49 @@ mod limit_buy_hook {
             let mut base_coins_to_withdraw = Decimal::ZERO;
 
             // For each order NFT in the bucket
-            for order_nft in order_bucket.as_non_fungible().non_fungibles::<LimitBuyOrderData>().iter() {
+            for order_nft in order_bucket.as_non_fungible().non_fungibles::<BuyOrderData>().iter() {
 
                 let order_data = order_nft.data();
 
-                let id = u32::try_from(
+                let order_id = u32::try_from(
                     match order_nft.local_id() {
                         NonFungibleLocalId::Integer(id) => id.value(),
                         _ => Runtime::panic("Should not happen".to_string()),
                     }
                 )
                 .unwrap();
-                let mut order = self.orders.get_mut(&id).unwrap();
 
-                let mut vault = self.coins_vaults.get_mut(&order_data.coin_to_buy).unwrap();
-                let bucket = vault.take(*order.get_bought_amount());
-                buckets.push(bucket);
+                let mut order_book = self.order_books.get_mut(&order_data.coin_to_buy).unwrap();
 
-                if coins_only {
-                    order.coins_withdrawn();
-                } else {
-                    let base_coins_in_this_order = order.get_base_coin_amount();
+                let coin_amount = match coins_only {
+                    true => order_book.withdraw_coins(&order_id),
+                    false => {
+                        let (base_coin_amount, coin_amount) = order_book.withdraw_all(&order_id);
 
-                    if  *base_coins_in_this_order > Decimal::ZERO {
-                        let mut active_orders = self.active_orders.get_mut(&order_data.coin_to_buy).unwrap();
-                        let order_ref = LimitBuyOrderRef::new(id, order_data.price); 
-                        let pos = active_orders.binary_search(&order_ref);
-                        match pos {
-                            Ok(pos) => { active_orders.remove(pos); },
-                            Err(_) => {},
-                        }
+                        base_coins_to_withdraw += base_coin_amount;
 
-                        base_coins_to_withdraw += *base_coins_in_this_order;
+                        coin_amount
                     }
+                };
+
+                if coin_amount > Decimal::ZERO {
+                    let mut vault = self.coins_vaults.get_mut(&order_data.coin_to_buy).unwrap();
+                    buckets.push(vault.take(coin_amount));
                 }
+            }
+
+            if base_coins_to_withdraw > Decimal::ZERO {
+                buckets.push(
+                    self.base_coin_vault.take_advanced(
+                        base_coins_to_withdraw,
+                        WithdrawStrategy::Rounded(RoundingMode::ToZero),
+                    )
+                );
             }
 
             if coins_only {
                 buckets.push(order_bucket);
             } else {
-                if base_coins_to_withdraw > Decimal::ZERO {
-                    buckets.push(self.base_coin_vault.take(base_coins_to_withdraw));
-                }
-
                 order_bucket.burn();
             }
 
@@ -369,64 +342,33 @@ mod limit_buy_hook {
                 return (hook_badge_bucket, None, vec![], vec![]);
             }
 
-            let mut active_orders = self.active_orders.get_mut(&argument.coin_address);
-            if active_orders.is_none() {
+            let order_book = self.order_books.get_mut(&argument.coin_address);
+            if order_book.is_none() {
                 return (hook_badge_bucket, None, vec![], vec![]);
             }
-            let active_orders = active_orders.as_mut().unwrap();
 
             let pool_info = argument.component.get_pool_info();
             
-            let mut base_coin_amount_so_far = Decimal::ZERO;
+            // Fee adjusted number of coins in the pool
+            let coin_in_pool = PreciseDecimal::from(pool_info.coin_amount) *
+                (100 - pool_info.total_buy_fee_percentage) / 100;
 
-            let mut first_non_filled_order_pos: usize = 0;
 
-            let mut partial_filled_order_amount = Decimal::ZERO;
+            let (
+                filled_amount,
+                filled_orders_id,
+                partially_filled_order_id,
+            ) = order_book.as_ref().unwrap().find_matches(
+                pool_info.base_coin_amount.into(),
+                coin_in_pool,
+            );
 
-            let mut partial_filled_order_id: Option<u32> = None;
-
-            let mut filled_orders_id: Vec<u32> = vec![];
-
-            for (pos, order_ref) in active_orders.iter().enumerate() {
-
-                if pos >= MAX_MATCHING_ORDERS {
-                    break;
-                }
-
-                // Compute the number of base coins I can spend to buy at the desired price or this
-                // order (can be less than zero if the price is higher than the desired one)
-                let base_coin_amount = pool_info.coin_amount * *order_ref.get_price() * ((100 - pool_info.total_buy_fee_percentage) / 100) - pool_info.base_coin_amount;
-
-                // If the orders with higher priority can already buy more than this amount, no deal
-                // for this order and the following
-                if base_coin_amount <= base_coin_amount_so_far {
-                    break;
-                }
-
-                let order = self.orders.get(order_ref.get_id()).unwrap();
-
-                if base_coin_amount - base_coin_amount_so_far > *order.get_base_coin_amount() {
-
-                    // Order filled
-                    first_non_filled_order_pos = pos + 1;
-                    base_coin_amount_so_far += *order.get_base_coin_amount();
-                } else {
-
-                    // Order partially filled
-                    partial_filled_order_amount = base_coin_amount - base_coin_amount_so_far;
-                    base_coin_amount_so_far = base_coin_amount;
-                    partial_filled_order_id = Some(*order_ref.get_id());
-
-                    break
-                }
-            }
-
-            if base_coin_amount_so_far == Decimal::ZERO {
+            if filled_amount == Decimal::ZERO {
                 return (hook_badge_bucket, None, vec![], vec![]);
             }
 
             let base_coin_bucket = self.base_coin_vault.take_advanced(
-                base_coin_amount_so_far,
+                filled_amount,
                 WithdrawStrategy::Rounded(RoundingMode::ToZero)
             );
 
@@ -435,25 +377,7 @@ mod limit_buy_hook {
                 || argument.component.buy(base_coin_bucket)
             );
 
-            let bought_price = base_coin_amount_so_far / coin_bucket.amount();
-
-            // Remove filled orders from self.active_orders
-            if first_non_filled_order_pos > 0 {
-                for order_ref in active_orders.drain(0..first_non_filled_order_pos as usize) {
-                    let id = order_ref.get_id();
-                    self.orders.get_mut(id).unwrap().fill(bought_price);
-                    filled_orders_id.push(*id);
-                }
-            }
-
-            // If an order has been partially filled now it is the first one, update its bought_amount
-            if partial_filled_order_amount > Decimal::ZERO {
-                let id = active_orders[0].get_id();
-                self.orders.get_mut(id).unwrap().partially_fill(
-                    partial_filled_order_amount,
-                    bought_price,
-                );
-            }
+            order_book.unwrap().swap(filled_amount, coin_bucket.amount());
 
             // Put the bought coins in self.coins_vaults
             let coin_vault = self.coins_vaults.get_mut(&argument.coin_address);
@@ -472,7 +396,7 @@ mod limit_buy_hook {
                 MatchedOrderEvent {
                     coin: argument.coin_address,
                     filled_orders_id: filled_orders_id,
-                    partially_filled_orders_id: partial_filled_order_id,
+                    partially_filled_orders_id: partially_filled_order_id,
                 }
             );
 
--- hooks/limit_buy/src/order.rs
+++ hooks/limit_buy/src/order.rs
@@ -1,117 +1,271 @@
 use scrypto::prelude::*;
 use std::cmp::Ordering;
+use std::rc::Rc;
 
-#[derive(ScryptoSbor)]
-pub struct LimitBuyOrder {
+#[derive(ScryptoSbor, Eq, PartialOrd)]
+pub struct Order {
+    id: u32,
+    price: Decimal,
     base_coin_amount: Decimal,
     bought_amount: Decimal,
 }
 
-impl LimitBuyOrder {
-    pub fn new(base_coin_amount: Decimal) -> LimitBuyOrder {
+impl Order {
+    pub fn new(
+        id: u32,
+        price: Decimal,
+        base_coin_amount: Decimal
+    ) -> Order {
+        assert!(
+            price > Decimal::ZERO,
+            "Price must be bigger than zero",
+        );
         assert!(
             base_coin_amount > Decimal::ZERO,
             "Base coin amount must be bigger than zero",
         );
 
         Self {
+            id: id,
+            price: price,
             base_coin_amount: base_coin_amount,
             bought_amount: Decimal::ZERO,
         }
     }
-
-    pub fn get_base_coin_amount(&self) -> &Decimal {
-        &self.base_coin_amount
-    }
-
-    pub fn get_bought_amount(&self) -> &Decimal {
-        &self.bought_amount
-    }
-
-    pub fn fill(
-        &mut self,
-        price: Decimal,
-    ) {
-        assert!(
-            price > Decimal::ZERO,
-            "Price must be bigger than zero",
-        );
-
-        self.bought_amount += self.base_coin_amount / price;
-        self.base_coin_amount = Decimal::ZERO;
-    }
-
-    pub fn partially_fill(
-        &mut self,
-        amount: Decimal,
-        price: Decimal,
-    ) {
-        assert!(
-            amount < self.base_coin_amount,
-            "Amount bigger than the available one",
-        );
-        assert!(
-            price > Decimal::ZERO,
-            "Price must be bigger than zero",
-        );
-
-        self.base_coin_amount -= amount;
-        self.bought_amount += amount / price;
-    }
-
-    pub fn coins_withdrawn(
-        &mut self,
-    ) {
-        self.bought_amount = Decimal::ZERO;
-    }
 }
 
-#[derive(ScryptoSbor, Eq, PartialOrd)]
-pub struct LimitBuyOrderRef {
-    id: u32,
-    price: Decimal,
-}
-
-impl LimitBuyOrderRef {
-    pub fn new(
-        id: u32,
-        price: Decimal,
-    ) -> LimitBuyOrderRef {
-        assert!(
-            price > Decimal::ZERO,
-            "Price must be bigger than zero",
-        );
-
-        Self {
-            id: id,
-            price: price,
-        }
-    }
-
-    pub fn get_id(&self) -> &u32 {
-        &self.id
-    }
-
-    pub fn get_price(&self) -> &Decimal {
-        &self.price
-    }
-}
-
-impl PartialEq for LimitBuyOrderRef {
+impl PartialEq for Order {
     fn eq(&self, other: &Self) -> bool {
-        self.price.eq(&other.price) && self.id.eq(&other.id)
+        self.id.eq(&other.id)
     }
 }
 
-// LimitBuyOrderRef are sortable by (reverse price, id)
-impl Ord for LimitBuyOrderRef {
+// Orders are sortable by (price, reverse id)
+impl Ord for Order {
     fn cmp(&self, other: &Self) -> Ordering {
-        let price_cmp = other.price.cmp(&self.price);
+        let price_cmp = self.price.cmp(&other.price);
 
         if price_cmp == Ordering::Equal {
-           self.id.cmp(&other.id)
+           other.id.cmp(&self.id)
         } else {
             price_cmp
         }
     }
+}
+
+#[derive(ScryptoSbor, Eq, PartialOrd)]
+pub struct OrderRef(Rc<RefCell<Order>>);
+
+impl OrderRef {
+    pub fn get_id(&self) -> u32 {
+        self.0.borrow().id
+    }
+
+    pub fn get_price(&self) -> Decimal {
+        self.0.borrow().price
+    }
+
+    pub fn get_base_coin_amount(&self) -> Decimal {
+        self.0.borrow().base_coin_amount
+    }
+}
+
+impl OrderRef {
+
+    pub fn new(
+        id: u32,
+        price: Decimal,
+        base_coin_amount: Decimal,
+    ) -> OrderRef {
+        let order = Order::new(id, price, base_coin_amount);
+
+        Self(Rc::new(RefCell::new(order)))
+    }
+
+    pub fn swap(
+        &mut self,
+        amount: Decimal,
+        ratio: Decimal,
+    ) {
+        let mut order = self.0.borrow_mut();
+
+        order.bought_amount += ratio * amount;
+        order.base_coin_amount -= amount;
+info!("swap #{}#, bought_amount: {}, base_coin_amount: {}, strong: {}, weak: {}", order.id, order.bought_amount, order.base_coin_amount, Rc::strong_count(&self.0), Rc::weak_count(&self.0));
+    }
+
+    pub fn swap_all(
+        &mut self,
+        ratio: Decimal,
+    ) {
+        let mut order = self.0.borrow_mut();
+
+        let order_base_coin_amount = order.base_coin_amount;
+        order.bought_amount += ratio * order_base_coin_amount;
+        order.base_coin_amount = Decimal::ZERO;
+info!("swap_all #{}#, bought_amount: {}, base_coin_amount: {}, strong: {}, weak: {}", order.id, order.bought_amount, order.base_coin_amount, Rc::strong_count(&self.0), Rc::weak_count(&self.0));
+    }
+}
+
+impl PartialEq for OrderRef {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.borrow().eq(&other.0.borrow())
+    }
+}
+
+impl Ord for OrderRef {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.borrow().cmp(&other.0.borrow())
+    }
+}
+
+impl Clone for OrderRef {
+    fn clone(&self) -> OrderRef {
+        Self(Rc::clone(&self.0))
+    }
+}
+
+#[derive(ScryptoSbor)]
+pub struct OrderBook {
+    orders: KeyValueStore<u32, OrderRef>,
+    active_orders: Vec<OrderRef>,
+}
+
+impl OrderBook {
+
+    pub fn new(order_ref: OrderRef) -> OrderBook {
+        // TODO: register type?
+        let orders = KeyValueStore::new();
+        let order_ref_clone = order_ref.clone();
+        orders.insert(order_ref.get_id(), order_ref_clone);
+
+        info!("strong: {}, weak: {}", Rc::strong_count(&order_ref.0), Rc::weak_count(&order_ref.0));
+
+        Self {
+            orders: orders,
+            active_orders: vec![order_ref],
+        }
+    }
+
+    pub fn insert(
+        &mut self,
+        order_ref: OrderRef,
+    ) {
+        self.orders.insert(order_ref.get_id(), OrderRef(Rc::clone(&order_ref.0)));
+
+        match self.active_orders.binary_search(&order_ref) {
+            Ok(_) => Runtime::panic("Should not happen".to_string()),
+            Err(pos) => self.active_orders.insert(pos, order_ref),
+        }
+    }
+
+    pub fn withdraw_coins(
+        &mut self,
+        order_id: &u32,
+    ) -> Decimal {
+        let order_ref = self.orders.get_mut(order_id).unwrap();
+        let mut order = order_ref.0.borrow_mut();
+
+        let coin_amount = order.bought_amount;
+
+        order.bought_amount = Decimal::ZERO;
+
+        coin_amount
+    }
+
+    pub fn withdraw_all(
+        &mut self,
+        order_id: &u32,
+    ) -> (
+        Decimal,
+        Decimal,
+    ) {
+        let order_ref = self.orders.remove(order_id).unwrap();
+        let order = order_ref.0.borrow();
+
+        match self.active_orders.binary_search(&order_ref) {
+            Ok(pos) => {
+                self.active_orders.remove(pos);
+            },
+            Err(_) => {},
+        }
+
+        (order.base_coin_amount, order.bought_amount)
+    }
+
+    pub fn find_matches(
+        &self,
+        base_coin_in_pool: PreciseDecimal,
+        coin_in_pool: PreciseDecimal,
+    ) -> (
+        Decimal, // Total base coin filled amount
+        Vec<u32>, // Matched orders id
+        Option<u32>, // Partially matched order id
+    ) {
+        let mut base_coin_amount_so_far = Decimal::ZERO;
+        let mut filled_orders_id: Vec<u32> = vec![];
+
+        for order_ref in self.active_orders.iter().rev() {
+            let machable_base_coin_amount = (coin_in_pool *order_ref.get_price() - base_coin_in_pool)
+                .checked_truncate(RoundingMode::ToZero)
+                .unwrap();
+
+            // No match for this order
+            if machable_base_coin_amount <= base_coin_amount_so_far {
+                return (base_coin_amount_so_far, filled_orders_id, None);
+            }
+
+            if machable_base_coin_amount - base_coin_amount_so_far >= order_ref.get_base_coin_amount() {
+
+                // Order filled
+                base_coin_amount_so_far += order_ref.get_base_coin_amount();
+                filled_orders_id.push(order_ref.get_id());
+info!("Filled: #{}#, base_coin_amount_so_far: {}", order_ref.get_id(), base_coin_amount_so_far);
+
+            } else {
+
+                // Order partially filled
+                let partially_filled_order_amount = machable_base_coin_amount - base_coin_amount_so_far;
+info!("Partially filled: #{}#, base_coin_amount_so_far: {}", order_ref.get_id(), base_coin_amount_so_far + partially_filled_order_amount);
+                return (base_coin_amount_so_far + partially_filled_order_amount, filled_orders_id, Some(order_ref.get_id()));
+            }
+        }
+
+        return (base_coin_amount_so_far, filled_orders_id, None);
+    }
+
+    pub fn swap(
+        &mut self,
+        base_coin_amount: Decimal,
+        coin_amount: Decimal,
+    ) {
+        let mut remaining_base_coin_amount = base_coin_amount;
+        let ratio = coin_amount / base_coin_amount;
+        let mut orders_to_remove = 0;
+
+        for (i, order_ref) in self.active_orders.iter_mut().rev().enumerate() {
+info!("i: {}", i);
+            if remaining_base_coin_amount == Decimal::ZERO {
+                orders_to_remove = i;
+                break;
+            }
+
+            if remaining_base_coin_amount >= order_ref.get_base_coin_amount() {
+
+                // Order filled
+                remaining_base_coin_amount -= order_ref.get_base_coin_amount();
+                order_ref.swap_all(ratio);
+
+            } else {
+
+                // Order partially filled
+                order_ref.swap(remaining_base_coin_amount, ratio);
+                orders_to_remove = i;
+                break;
+            }
+        }
+
+        self.active_orders.truncate(self.active_orders.len() - orders_to_remove);
+    }
+
 }
